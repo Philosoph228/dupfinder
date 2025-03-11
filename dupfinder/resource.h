bda7e051c922777619be158a221f9711c0db4b65
Philosoph228 <philosoph228@gmail.com>
2025-03-11 05:37:46 +0500

    Implement thumbcache

--- dupfinder/resource.h
+++ dupfinder/resource.h
@@ -5,12 +5,15 @@
 #define IDD_DIALOG1                     101
 #define IDR_MENU1                       103
 #define IDI_ICON1                       105
+#define IDR_MENU2                       106
+#define IDD_DIALOG2                     107
 #define IDC_EDIT1                       1001
 #define IDC_BUTTON1                     1002
 #define IDC_PROGRESS1                   1003
-#define IDC_BUTTON2                     1004
+#define IDC_FINDDUPLICATESBTN           1004
 #define IDC_EDIT2                       1005
 #define IDC_LIST1                       1006
+#define IDC_CANCELTASKSBTN              1007
 #define ID_VIEW_ICONS                   40001
 #define ID_VIEW_LIST                    40002
 #define ID_VIEW_DETAILS                 40003
@@ -20,14 +23,15 @@
 #define ID_FILE_CREATE                  40007
 #define ID_FILE_COPY                    40008
 #define ID_FILE_CUT                     40009
+#define ID_DEBUG_VIEWLISTVIEWTHUMBNAILIMAGELIST 40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40010
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
